97415a9df862b6ae46fb209ad3a9b8b2ced761e5
Amanda Bertuccelli <akbertuccelli@gmail.com>
2022-07-20 17:05:52 -0700

    initial commit for Product Manager Part I

---
+++ server/models/product.model.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const ProductSchema = new mongoose.Schema({
+    title : {type: String},
+    price : {type: Number},
+    description : {type: String}
+}, {timestamps: true});
+// module.exports.ProductSchema = mongoose.model('Product', ProductSchema);
+const Product = mongoose.model('Product', ProductSchema);
+module.exports = Product;
