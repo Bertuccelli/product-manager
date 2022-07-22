a065af28c0cbf2f201ea6c62f06cad817fbd5a5f
Amanda Bertuccelli <akbertuccelli@gmail.com>
2022-07-21 19:16:22 -0700

    commit for Product Manager Part III

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -24,3 +24,15 @@ module.exports.getProduct =(req, res) => {
         .then(product => res.json(product))
         .catch(err => res.json(err))
 }
+
+module.exports.updateProduct = (req, res) => {
+    Product.findOneAndUpdate({_id: req.params.id}, req.body, {new:true})
+        .then(updateProduct => res.json(updateProduct))
+        .catch(err => res.json(err))
+}
+
+module.exports.deleteProduct = (req, res) => {
+    Product.deleteOne({_id: req.params.id})
+        .then(message => res.json(message))
+        .catch(err=> res.json(err))
+}
